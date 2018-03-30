b3e0511c31656cc5144345abd1d8ceba9f8f3771
tinuola <tinuola.awopetu@gmail.com>
2018-03-30 16:38:45 -0700

    animate portfolio imgs

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
   $('.burger-nav').on('click', function(){
       $('.container nav ul').toggleClass('open');
   });
+
+  $('.portfolio-imgs').on('mouseenter', function(){
+    $(this).children().fadeIn(400).css('display', 'inherit');
+
+  }).on('mouseleave', function(){
+    $('.hide').css('display', 'none');
+  });
+
 });
