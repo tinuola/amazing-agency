ce9a216401949679c0dc97507a3f72c8af30f2d5
tinuola <tinuola.awopetu@gmail.com>
2018-04-01 17:53:57 -0700

    adding animated counting

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-
+  //
   new WOW().init();
 
+  //
   $('.burger-nav').on('click', function(){
       $('.container nav ul').toggleClass('open');
   });
 
+  //
   $('.portfolio-imgs').on('mouseenter', function(){
     $(this).children().fadeIn(400).css('display', 'inherit');
-
   }).on('mouseleave', function(){
     $('.hide').css('display', 'none');
+  });
+
+  //
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000,
+    offset: 100,
+    beginAt: 0
   });
 
 });
