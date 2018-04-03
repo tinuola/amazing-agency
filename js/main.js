c3e640bd8e0018adae49d3168dbec15ac03cb8be
tinuola <tinuola.awopetu@gmail.com>
2018-04-02 23:51:24 -0700

    adding smooth scroll

--- js/main.js
+++ js/main.js
@@ -2,19 +2,40 @@ $(document).ready(function(){
   //
   new WOW().init();
 
-  //
+  //hamburger menu
   $('.burger-nav').on('click', function(){
       $('.container nav ul').toggleClass('open');
   });
 
-  //
+  //smooth scrolling
+  $('a[href*="#"]')
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .on('click', function(event) {
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      let hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  //hover effect on portfolio section images
   $('.portfolio-imgs').on('mouseenter', function(){
     $(this).children().fadeIn(400).css('display', 'inherit');
   }).on('mouseleave', function(){
     $('.hide').css('display', 'none');
   });
 
-  //
+  //animate stats section counter
   $('.counter').counterUp({
     delay: 10,
     time: 1000,
